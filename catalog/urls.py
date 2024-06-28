39bc5b541364a2d4f958c4dbdb00a9c82bd93eb3
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2024-06-28 16:40:13 +0300

    Маршруты

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from catalog.views import main
 from catalog.views import about
+from catalog.views import category
+from catalog.views import device
 
 urlpatterns = [
+    path('main/<str:category>/<int:index>', device),
+    path('main/<str:category>/', category),
     path('main/', main),
     path('about/', about)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, HttpResponseNotFound
 
 
 # Create your views here.
 def main(request: HttpRequest):
+
     return HttpResponse(
         content=""" <h1> Добро пожаловать на главную страницу catalog </h1> """
+    )
+
+
+def category(request: HttpRequest, category=None):
+    if category not in ('phones', 'laptop'):
+        return HttpResponse(
+            content=f""" <h1> Категория {category} не найдена </h1> """
+        )
+    device_name = request.GET.get('device_name')
+    model = request.GET.get('model')
+
+    return HttpResponse(
+        content=f""" 
+        <h1> Добро пожаловать на страницу {category} нашего catalog </h1> 
+        <p>Нзвание устройства: {device_name}</p>
+        <p>Модель: {model}</p>
+        """
+    )
+
+
+def device(request: HttpRequest, category=None, index=None):
+    if index not in (123456, 123457):
+        return HttpResponse(
+            content=f""" <h1> Устройство {index} в категории {category} не найдено </h1> """
+        )
+
+    return HttpResponse(
+        content=f""" <h1> Добро пожаловать на страницу устройства №{index} нашего catalog </h1> """
     )
 
 
